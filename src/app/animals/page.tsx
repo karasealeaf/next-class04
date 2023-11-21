013c39a1270540be08f4e21d4e8b6c8c35cd00e4
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-21 11:18:25 +0000

    asc and desc

--- src/app/animals/page.tsx
+++ src/app/animals/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+type petsSearchQuery = {
+  sortBy: string;
+};
+
 type petsType = { name: string; id: number };
 
 let pets: petsType[] = [
@@ -30,19 +34,52 @@ let pets: petsType[] = [
   {
     name: "Reginald",
     id: 6,
-  }
+  },
 ];
 
-export default function Home() {
+function comparePets(a: petsType, b: petsType) {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: petsSearchQuery;
+}) {
+  let sortedPets = [...pets];
+
+  if (searchParams.sortBy == "asc") {
+    sortedPets.sort(comparePets);
+  } else if (searchParams.sortBy == "desc") {
+    sortedPets.sort(comparePets).reverse();
+  }
+
   return (
     <div>
       <h4>This is a website about animals: here are many animals!</h4>
-      {pets.map(pet => {
+      <p>{searchParams.sortBy}</p>
+      <br />
+      <Link href="/animals">Remove the sort</Link>
+      <br />
+      <Link href="/animals?sortBy=asc">Sort by ascending</Link>
+      <br />
+      <Link href="/animals?sortBy=desc">Sort by descending</Link>
+      <br />
+      {sortedPets.map((pet) => {
         return (
           <div key={pet.id}>
-          <br />
-          <Link href={`/animals/${pet.name.toUpperCase()}`}>{pet.name}</Link> </div>
-        )
+            <br />
+            <Link href={`/animals/${pet.name.toLowerCase()}`}>
+              {pet.name}
+            </Link>{" "}
+          </div>
+        );
       })}
     </div>
   );
